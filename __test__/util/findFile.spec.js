888237127a75c243f06c2d00e94e7332bcc5f4a4
shuyibin <18291954875@163.com>
2019-12-26 21:16:35 +0800

    feat: add jest

---
+++ __test__/util/findFile.spec.js
@@ -0,0 +1,25 @@
+const { findFiles } = require("../../lib/util/findFile");
+
+describe("findFile", () => {
+  it("test find pack file", () => {
+    const file = ["examples/raw/test/test.js"];
+    return findFiles("examples/raw").then(data => {
+      expect(data).toEqual(file);
+    });
+  });
+  it("test can not find pack file", () => {
+    // expect.assertions(1);
+    const file = [];
+    return findFiles("domytest")
+    .then(data => {
+      expect(data).toEqual(file);
+    });
+    // .catch(e => expect(e).toMatch('error'))
+  });
+  it('test buy async/await', async () => {
+    expect.assertions(1)
+    const file = ["examples/raw/test/test.js"];
+    const data = await findFiles("examples/raw")
+    expect(data).toEqual(file);
+  })
+});
