d03835d31f44c289649515475c57a51f2de4df2c
刘放 <362512489@qq.com>
2020-03-04 11:44:38 +0800

    test: run test and change version

--- __test__/build/dist/jsx-test/test.js
+++ __test__/build/dist/jsx-test/test.js
@@ -1,7 +1,7 @@
 /**
 * ------------------------------------------
 * Rollup vue to nej
-* @version  1.2.4
+* @version  1.2.5
 * @path  __test__/build/dist/jsx-test/test.js
 * 
 * You need to check the changes after packing
